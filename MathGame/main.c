3ef4625ad3fbbc629016e16fde175bb5e54ddcda
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-05 20:33:41 -0700

    added pseudocode

--- MathGame/main.c
+++ MathGame/main.c
@@ -8,7 +8,33 @@
 
 #include <stdio.h>
 
+//int playerOneLives;
+//int playerTwoLives;
+//int activePlayer;
+
 int main(int argc, const char * argv[]) {
+    
+    /* 
+     set lives of players to 3
+     loop while (player one and player two both have lives)
+     {
+        define first number randomly between 1 and 20;
+        define second number randomly between 1 and 20;
+     
+        prompt active player;
+        take input from active player;
+        evaluate input vs actual result;
+        if result is correct
+            display affirming message about life
+        if result is incorrect
+            display disparaging message
+            decrement lives
+            display scores
+        switch active player
+     }
+     announce winner
+     display scores
+     */
     
     return 0;
 }
