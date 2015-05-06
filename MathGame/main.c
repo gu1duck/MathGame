996f700ba41c1b69b99027b91a404a35c57877ad
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-05 21:03:45 -0700

    added random numbers that generate randomly every time.

--- MathGame/main.c
+++ MathGame/main.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int playerOneLives;
 int playerTwoLives;
@@ -26,6 +27,7 @@ int main(int argc, const char * argv[]) {
     /*
         define first number randomly between 1 and 20;
      */
+         srand((unsigned)time(NULL));
          int firstNumber;
          int secondNumber;
          
