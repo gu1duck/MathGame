18998dd39a32f276b3efd4f0e85c031e1c2154f4
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-05 20:19:34 -0700

    initial commit; added empty project and README

---
+++ MathGame/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MathGame
+//
+//  Created by Jeremy Petter on 2015-05-05.
+//  Copyright (c) 2015 Jeremy Petter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
